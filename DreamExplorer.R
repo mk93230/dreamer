505c1877f3f893d7ec72a5c635629ff2c1f98d24
mk93230 <kamalsujatha@gmail.com>
2018-12-09 21:06:40 -0500

    Improvements were made to the DreamExplorer file. Year value was included. Anything with NA has been removed. Utilities file can get year from the createdate text.

--- DreamExplorer.R
+++ DreamExplorer.R
@@ -8,8 +8,9 @@ library(knitr)
 library(tidyverse)
 
 # Including other source files
-source("Cleaner.R")
+source("Cleaner")
 source("DataRetreiver")
+source("Utilities")
 
 DreamExtract <- ExtractDreams()
 
@@ -29,4 +30,22 @@ DreamExtract$Scene1 <- sapply(DreamExtract$Scene1, tolower)
 
 # Check the work
 str(DreamExtract[139, ]$Scene1, nchar.max = 1000)
+
+dreamer <- DreamExtract %>% mutate(year=getYear(DreamExtract$Created_dDate))
+
+# Remove all NA's from the year by filtering the data
+dreamer <- dreamer %>% filter(!is.na(dreamer$year))
+
+
+dreamer %>%
+  group_by(year) %>%
+  summarise(number_of_dreams = n()) %>%
+  ggplot(aes(x = year, y = number_of_dreams,color="red")) + 
+  geom_bar(stat = "identity")  +
+  theme(plot.title = element_text(hjust = 0.5),
+        legend.title = element_blank(),
+        panel.grid.minor = element_blank()) +
+  ggtitle("Dream Stories") +
+  labs(x = "Year", y = "Number of Dreams")
+
 
--- Utilities.R
+++ Utilities.R
@@ -1,4 +1,9 @@
 # String to data function
-stringtoDate<-function(x){
+stringToDate<-function(x){
   mdy_hm(x)
+}
+
+getYear <- function(x){
+  date_value=mdy_hm(x)
+  return(year(date_value))
 }
