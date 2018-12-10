c2119c11c08da4b1b97eec965dd7aa334cc931e7
mk93230 <kamalsujatha@gmail.com>
2018-12-09 21:06:40 -0500

    Improvements were made to the DreamExplorer file. Year value was included. Anything with NA has been removed. Utilities file can get year from the createdate text.

--- DataRetreiver.R
+++ DataRetreiver.R
@@ -3,6 +3,7 @@
 ExtractDreams <- function(){
   DreamExtract <- read.csv("C:/Kamal/R/projects/eyes2C/data/raw/DreamExtract.csv", header=FALSE)
   DreamExtract <- setNames(DreamExtract,c("Id","Name","Scene1","Scene2","Scene3","Scene4","Scene5","Created_dDate"))
+  
   #DreamExtractList <- split(DreamExtract,seq(nrow(DreamExtract)))
   #DreamExtractList <- DreamExtractList[-1]
   return(DreamExtract)
