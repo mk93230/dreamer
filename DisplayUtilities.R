026dfd55c6a5907cc62c59163bb1d0f1b0a50eb8
mk93230 <kamalsujatha@gmail.com>
2019-02-12 04:10:13 -0500

    Built 8 topics. Also created DisplayUtilities script file for wordchat and tidy model

---
+++ DisplayUtilities.R
@@ -0,0 +1,52 @@
+# Dream for Wellness - Kamal & Jeff
+# @author Kamal
+# utilities for display
+num_words <- 10
+
+# Create a function that accepts lda model and number of words
+
+top_terms_per_topic <- function(lda_model, num_words)
+{
+  # tidy lda object to get topics, words and probability (beta)
+  lda_tidy <- tidy(lda,matrix="beta")
+  top_terms <- lda_tidy %>% group_by(topic) %>% arrange(topic,desc(beta)) %>%
+    #get the top num_words PER topic
+    slice(seq_len(num_words)) %>%
+    arrange(topic, beta) %>%
+    #row is required for the word_chart() function
+    mutate(row = row_number()) %>%
+    ungroup() %>%
+    #add the word Topic to the topic labels
+    mutate(topic = paste("Topic", topic, sep = " "))
+  #create a title to pass to word_chart
+  title <- paste("LDA Top Terms for", k, "Topics")
+  #call the word_chart function you built in prep work
+  word_chart(top_terms, top_terms$term, title)
+  
+}
+
+
+word_chart <- function(data, input, title) {
+  data %>%
+    #set y = 1 to just plot one variable and use word as the label
+    ggplot(aes(as.factor(row), 1, label = input, fill = factor(topic) )) +
+    #you want the words, not the points
+    geom_point(color = "transparent") +
+    #make sure the labels don't overlap
+    geom_label_repel(nudge_x = .2,  
+                     direction = "y",
+                     box.padding = 0.1,
+                     segment.color = "transparent",
+                     size = 3) +
+    facet_grid(~topic) +
+    theme_lyrics() +
+    theme(axis.text.y = element_blank(), axis.text.x = element_blank(),
+          #axis.title.x = element_text(size = 9),
+          panel.grid = element_blank(), panel.background = element_blank(),
+          panel.border = element_rect("lightgray", fill = NA),
+          strip.text.x = element_text(size = 9)) +
+    labs(x = NULL, y = NULL, title = title) +
+    #xlab(NULL) + ylab(NULL) +
+    #ggtitle(title) +
+    coord_flip()
+}
