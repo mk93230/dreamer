d07aa36cefa31a2e72c3d29172b6f9c12dbb2b4a
mk93230 <kamalsujatha@gmail.com>
2019-02-17 12:41:46 -0500

    Created Unsupervised learning report. Top 30 words for each topic.

--- MachineLearningExercise.R
+++ MachineLearningExercise.R
@@ -1,8 +1,17 @@
-#linstall.packages("caret")
+#install.packages("caret")
+#install.packages("dslabs")
+# Galton Heights
+#install.packages("HistData")
+# Machine learning library
 library(caret)
 # Heights dataset
-install.packages("dslabs")
 library("dslabs")
 data(heights)
 y <- heights$sex
 x<- heights$height
+
+library(HistData)
+galton_heights <- GaltonFamilies %>% 
+  filter(childNum == 1 & gender == "male") %>%
+  select(father, childHeight) %>% 
+  rename(son = childHeight)
